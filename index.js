3eaddaaf0cf1f58b1d2f6086de694365fa9a940b
Ayush Mhetre <mhetreayush1719@gmail.com>
2022-02-27 09:52:25 +0530

    Mobile functionality added

---
+++ index.js
@@ -0,0 +1,9 @@
+function showNavContent(){
+    document.getElementsByClassName("pcNav").style="display:none";
+    document.getElementById("toggleBarContent").style="display:block;";
+}
+
+function hideNavContent(){
+    document.getElementsByClassName("pcNav").style="display:block";
+    document.getElementById("toggleBarContent").style="display:none;";
+}
